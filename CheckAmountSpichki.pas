10e0fc76d2300e9832b8fc6420d90b53a2909e0a
og-solo <semyonsolonichev@gmail.com>
2020-11-27 19:50:25 +0300

    Implemented Player choice procedure

---
+++ CheckAmountSpichki.pas
@@ -0,0 +1,9 @@
+﻿unit CheckAmountSpichki; // Semen
+
+procedure CheckAmountSpichki(var Spichki : integer);
+begin
+  Spichki := ReadInteger('Выберите кол-во спичек :');
+  Assert(Spichki > 0, 'Выберите 1 или больше');
+  Assert(Spichki < 5, 'Выберите 4 или меньше');
+end;
+end.
